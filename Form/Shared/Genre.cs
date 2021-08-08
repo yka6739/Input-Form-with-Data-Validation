c213de84abac61e0d2e1243ded3c9ec3e4da9bd0
YATENDRA AWANA <yka6739@gmail.com>
2021-08-08 19:43:04 +0530

    Add project files.

---
+++ Form/Shared/Genre.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Form.Shared
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "This Name Field is Required.........")]
+        public string FatherName { get; set; }
+        [Required]
+        public string MotherName { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Pincode { get; set; }
+    }
+}
